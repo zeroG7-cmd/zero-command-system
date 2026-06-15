00cd8fb9b9c197f1685924784fabbf6c19b515df
zeroG7-cmd <zerogravity845@gmail.com>
2026-06-15 11:26:29 +0100

    Add main function to run show_dashboard

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ def show_dashboard():
     print("=================================")
     print("ZERO COMMAND SYSTEM")
     print("=================================")
+
+
+
+def main():
+    show_dashboard()
+
+
+if __name__ == "__main__":
+    main()
